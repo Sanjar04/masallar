2c92c5b008555f5ba74a9c5f2fc5b36e362123fb
Sanjar04 <sanjarpolatov200204@gmail.com>
2022-08-12 16:34:37 -0900

    masallar

---
+++ js/main.js
@@ -0,0 +1,173 @@
+// 1-masala 
+// let j_son = +prompt("Enter number");
+// if(j_son > 0 ){
+//    alert("A son musbat")
+// }else{
+//    alert("Bu son manfiy")
+// }
+
+// 2-3-masala 
+//  let k_son = +prompt("Enter number");
+//  if (k_son % 2 == 0) {
+//   alert("A son juft");
+//  } else if (k_son==1){
+//    alert("son juft ham emas toq ham emas")
+//  }else{
+//    alert("A son toq");
+//  }
+
+// 4-masala
+// let l_son = +prompt("enter_the_first_number");
+// let m_son = +prompt("enter_the_second_number");
+// if (l_son > 0 && m_son > 0) {
+//     alert("numbers are positive")
+// } else if (l_son < 0 && m_son > 0) {
+//     alert("the first number is negative, the second number is positive")
+// } else if (l_son > 0 && m_son < 0) {
+//     alert("the first number is positive, the second number is negative")
+// } else {
+//     alert("numbers are negative")
+// }
+
+// 5-masala
+// let n_son = +prompt("a sonni kiriting: ");
+// let o_son = +prompt("b sonni kiriting: ");
+// let p_son = +prompt("c sonni kiriting: ");
+// if (n_son > o_son && o_son > p_son){
+//    alert("true")
+// }else{
+//    alert("false")
+// }
+
+// 6-masala
+// let q_son = +prompt("a sonni kiriting: ");
+// let r_son = +prompt("b sonni kiriting: ");
+// let s_son = +prompt("c sonni kiriting: ");
+// if (q_son < r_son < s_son){
+//    alert("true")
+// }else{
+//    alert("false")
+// }
+
+// 7-masala
+// let t_son = +prompt("a sonni kiriting: ");
+// let u_son = +prompt("b sonni kiriting: ");
+// if(t_son % 2 == 0 && u_son % 2 == 0){
+//    alert("false")
+// }else{
+//    alert("true")
+// }
+
+// 8-masala
+// let u_son = +prompt("a sonni kiriting: "),
+//   v_son = +prompt("b sonni kiriting: "),
+//   w_son = +prompt("c sonni kiriting: "),
+//   x_son = +prompt("d sonni kiriting: ");
+// if (u_son > 0 && v_son > 0 && w_son > 0 && x_son > 0) {
+//   alert("true");
+// } else {
+//   alert("false");
+// }
+
+// 9-masala
+// let y_son = +prompt("a sonni kiriting: "),
+//   z_son = +prompt("b sonni kiriting: "),
+//   a_son = +prompt("c sonni kiriting: ");
+// if (y_son > 0 && z_son > 0 && a_son < 0) {
+//    alert("true")
+// } else if (y_son < 0 && z_son > 0 && a_son > 0) {
+//    alert("true")
+// } else if (y_son > 0 && z_son < 0 && a_son > 0) {
+//    alert("true")
+// } else {
+//   alert("false");
+// }
+
+// 10-masala
+// let b_son = +prompt("Hafta kunini son orqali kiriting: ");
+// if (b_son == 1) {
+//   alert("Dushanba");
+// } else if (b_son == 2) {
+//   alert("Seshanba");
+// } else if (b_son == 3) {
+//   alert("Chorshanba");
+// } else if (b_son == 4) {
+//   alert("Payshanba");
+// } else if (b_son == 5) {
+//   alert("Juma");
+// } else if (b_son == 6) {
+//   alert("Shanba");
+// } else if (b_son == 7) {
+//   alert("Yakshanba");
+// }
+
+// 11-masala
+// let c_son = +prompt("Company Uzbekistan");
+// if (c_son == 90 && c_son == 91) {
+//   alert("BEELINE");
+// } else if (c_son == 97 && c_son == 98) {
+//   alert("MobiUz");
+// } else if (c_son == 94 && c_son == 93) {
+//   alert("UCELL");
+// } else if (c_son == 99 && c_son == 95) {
+//   alert("UzMobile");
+// } else if (c_son == 98) {
+//   alert("Perfectum");
+// }else{
+//    alert("?????")
+// }
+
+// 12-masala
+// let d_son = +prompt("Enter number: ");
+// if (d_son > 0) {
+//   d_son++;
+//   alert(d_son);
+// } else {
+//   d_son--;
+//   alert(d_son);
+// }
+
+// 13-masala
+// let e_son = +prompt("Enter number: ");
+// if (e_son > 0) {
+//   alert(e_son + 3);
+// } else {
+//   alert(e_son - 2);
+// }
+
+// 14-masala
+// let x_son = +prompt("Enter number: ");
+// let y_son = +prompt("Enter number: ");
+// if (x_son > y_son) {
+//   alert(x_son);
+// } else {
+//   alert(y_son);
+// }
+
+// 15-masala
+// let a = +prompt("Enter number_1: "),
+//     b = +prompt("Enter number_2: "),
+//     c = +prompt("Enter number_3: ");
+// if(a > b && a > c){
+// alert( a + " son katta")
+// }else if( b > a && b > c){
+//    alert( b + " soni katta")
+// }else if (c > a && c > b){
+//    alert(c + " soni katta")
+// }else{
+//    alert("????")
+// }
+
+// 16-masala
+// let a = +prompt("Enter number_1: "),
+//   b = +prompt("Enter number_2: "),
+//   c = +prompt("Enter number_3: ");
+// if (a < b && a < c) {
+//   alert(a + " soni kichik");
+// } else if (b < a && b < c) {
+//   alert(b + " soni kichik");
+// } else if (c < a && c < b) {
+//   alert(c + " soni kichik");
+// } else {
+//   alert("????");
+// }
